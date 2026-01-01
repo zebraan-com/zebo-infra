4337d0a7fd0480903dc7aa3e720fb105a4c08c96
Aninda Chakraborty <aninda.death@gmail.com>
2026-01-01 12:13:52 +0530

    adding  terraform variables in github actions: Setting up srtifact registry secret in terraform

---
+++ terraform/modules/kubernetes/artifact_registry_secret.tf
@@ -0,0 +1,55 @@
+resource "kubernetes_secret" "artifact_registry_secret" {
+  metadata {
+    name      = "artifact-registry-secret"
+    namespace = var.namespace
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  data = {
+    ".dockerconfigjson" = jsonencode({
+      auths = {
+        "${var.artifact_registry_host}" = {
+          "auth" = base64encode("_json_key:${file(var.service_account_key_path)}")
+        }
+      }
+    })
+  }
+
+  depends_on = [
+    # Add any dependencies here, such as the Kubernetes provider being configured
+  ]
+}
+
+# Patch the default service account to use the image pull secret
+resource "kubernetes_service_account" "default" {
+  metadata {
+    name      = "default"
+    namespace = var.namespace
+  }
+
+  image_pull_secret {
+    name = kubernetes_secret.artifact_registry_secret.metadata[0].name
+  }
+
+  # This prevents Terraform from trying to manage the default service account's secrets
+  # which are managed by Kubernetes
+  automount_service_account_token = false
+}
+
+# Variables
+variable "artifact_registry_host" {
+  description = "The hostname of the Artifact Registry (e.g., asia-south1-docker.pkg.dev)"
+  type        = string
+}
+
+variable "service_account_key_path" {
+  description = "Path to the service account key file"
+  type        = string
+}
+
+variable "namespace" {
+  description = "The namespace where to create the secret"
+  type        = string
+  default     = "default"
+}
