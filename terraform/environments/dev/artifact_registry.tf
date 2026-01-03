158d80ee154e176a8bc1147b496b390ba53b3007
Aninda Chakraborty <aninda.death@gmail.com>
2026-01-03 18:08:41 +0530

    adding  terraform variables in github actions: Setting up srtifact registry secret in terraform

--- terraform/environments/dev/artifact_registry.tf
+++ terraform/environments/dev/artifact_registry.tf
@@ -1,13 +1,13 @@
 module "artifact_registry_secret" {
   source = "../../modules/kubernetes"
-  
+
   artifact_registry_host = "asia-south1-docker.pkg.dev"
   service_account_key_path = "${path.module}/../../service-account-key.json"
-  namespace = "default"
-  
+  namespace              = "default"
+
   # Ensure the Kubernetes provider is properly configured before creating this resource
   depends_on = [
     # Add any dependencies like your GKE cluster here
-    # google_container_cluster.primary
+    google_container_cluster.primary,
   ]
 }
