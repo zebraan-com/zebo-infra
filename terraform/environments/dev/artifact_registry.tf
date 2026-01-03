0f8f22a4712f94fdb74194d8982fc54f2f799148
Aninda Chakraborty <aninda.death@gmail.com>
2026-01-03 18:22:13 +0530

    adding  terraform variables in github actions: fixing fmt  in terraform

--- terraform/environments/dev/artifact_registry.tf
+++ terraform/environments/dev/artifact_registry.tf
@@ -2,7 +2,7 @@ module "artifact_registry_secret" {
   source = "../../modules/kubernetes"
 
   artifact_registry_host   = "asia-south1-docker.pkg.dev"
-  service_account_key_path = "${path.module}/../../service-account-key.json"
+  service_account_key_path = "${path.module}/../../../../service-account-key.json"
   namespace                = "default"
 
   # The Kubernetes provider configuration will be handled by the module
