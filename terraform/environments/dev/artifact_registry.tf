fa53fafd12b783a6f68b7b3f61dba9b32b0d789d
Aninda Chakraborty <aninda.death@gmail.com>
2026-01-04 16:27:59 +0530

    adding  terraform variables in github actions: fixing fmt  in terraform

--- terraform/environments/dev/artifact_registry.tf
+++ terraform/environments/dev/artifact_registry.tf
@@ -1,10 +1,10 @@
-# Configure Kubernetes service account to use Workload Identity
-resource "kubernetes_service_account_v1" "default" {
-  metadata {
-    name      = "default"
-    namespace = "default"
-    annotations = {
-      "iam.gke.io/gcp-service-account" = var.gke_node_pool_sa_email
-    }
-  }
-}
+# # Configure Kubernetes service account to use Workload Identity
+# resource "kubernetes_service_account_v1" "default" {
+#   metadata {
+#     name      = "default"
+#     namespace = "default"
+#     annotations = {
+#       "iam.gke.io/gcp-service-account" = var.gke_node_pool_sa_email
+#     }
+#   }
+# }
