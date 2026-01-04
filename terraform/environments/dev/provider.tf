a24b4b74c0b8dbf893163402700019c092f56c55
Aninda Chakraborty <aninda.death@gmail.com>
2026-01-04 16:56:07 +0530

    adding  terraform variables in github actions: fixing fmt  in terraform

--- terraform/environments/dev/provider.tf
+++ terraform/environments/dev/provider.tf
@@ -6,19 +6,21 @@ provider "google" {
 }
 
 # Get GKE cluster information
-data "google_container_cluster" "primary" {
-  name     = "gke-cluster" # Using a more generic name that might match your cluster
-  location = var.region
-}
+# data "google_container_cluster" "primary" {
+#   name     = "gke-cluster" # Using a more generic name that might match your cluster
+#   location = var.region
+# }
 
 # Configure Kubernetes provider to connect to the GKE cluster
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.primary.endpoint}"
+  host                   = "https://${google_container_cluster.primary.endpoint}"
   token                  = data.google_client_config.provider.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  )
 }
 
-data "google_client_config" "provider" {}
+data "google_client_config" "default" {}
 
 # Uncomment and configure a remote backend if you want remote state Update the bucket name as ${PROJECT_ID}-terraform-state 
 terraform {
